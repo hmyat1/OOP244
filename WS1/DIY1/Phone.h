9230325bcc2251e17bd2c28e6964f1540cb0e3e6
hmyat1 <132102780+hmyat1@users.noreply.github.com>
2023-10-07 17:28:22 -0400

    Add files via upload

---
+++ WS1/DIY1/Phone.h
@@ -0,0 +1,22 @@
+/* ------------------------------------------------------
+Workshop 1 part 2
+Module: directory
+Filename: Phone.h
+Version 1
+Author	Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date  12.9.23     Reason
+-----------------------------------------------------------*/
+#ifndef NAMESPACE_PHONE_H
+#define NAMESPACE_PHONE_H
+#include "cStrTools.h"
+using namespace std;
+
+namespace sdds
+
+{
+    void phoneDir(const char* programTitle, const char* fileName);
+}
+
+#endif
---
+++ WS1/DIY1/directory.cpp
@@ -0,0 +1,18 @@
+/* ------------------------------------------------------
+Workshop 1 part 2
+Module: directory
+Filename: directory.cpp
+Version 1
+Author	Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date   12.9.23    Reason
+-----------------------------------------------------------*/
+#include <iostream>
+#include "Phone.h"
+using namespace sdds;
+int main() 
+{
+	phoneDir("Star Wars", "phones.txt");
+	return 0;
+}
