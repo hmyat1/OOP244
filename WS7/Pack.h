e2093f342a646b49e9e2c6e8341f42239b69f905
hmyat1 <vermaishita334@gmail.com>
2023-11-07 22:42:39 -0500

    Add files via upload

---
+++ WS7/Pack.h
@@ -0,0 +1,37 @@
+#ifndef PACK_H
+#define PACK_H
+
+#include "Container.h"
+using namespace sdds;
+
+namespace sdds {
+    class Pack : public Container {
+    private:
+        int Unit_Size;
+        int Size;
+        int Number_Of_Units;
+
+
+    public:
+        Pack(const char* Content, int Size, int unitSize = 330, int NumberOfUnits = 0);
+        int operator += (int units);
+        int operator -= (int units);
+        int unit();
+        int noOfUnits();
+        int size();
+        void clear(int packSize, int unitSize, const char* content);
+        ostream& print(ostream& out);
+        istream& read(istream& in);
+        friend ostream& operator << (ostream& out, Pack& P) {
+            return P.print(out);
+        }
+
+        friend istream& operator >> (istream& in, Pack& P) {
+            return P.read(in);
+        }
+
+    };
+}
+
+
+#endif
